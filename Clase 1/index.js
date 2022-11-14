433e00d12976c98a15458e376f7fbb0a3742ee16
nabarca1388 <nicolas.m.abarca@gmail.com>
2022-11-13 22:49:46 -0300

    clase 1

---
+++ Clase 1/index.js
@@ -0,0 +1,34 @@
+//Comentario de linea en Java-script
+/*Comentario de bloque */
+//Usar camel case
+//Declaracion de variable
+
+/*let nombre = 5; //Recomendable
+let apodo = "copito de nieve"; //string
+var apellido = 7; //No recomendable
+
+console.log(nombre); //Es para mostrar por consola
+console.log("Hola trolaaaa");
+console.log(typeof(apodo)); //Muestra por consola el tipo de dato de la variable que se pida
+console.log(typeof(apellido));
+console.log(typeof(nombre));
+
+const NUMERO = 5;   //constantes
+
+let numero1 = 3;
+let numero2 = 2;
+let resultado = numero1 + numero2;
+console.log(resultado);
+
+let saludo = "Hola";
+let nombreSaludo = "Roberto";
+let mensajeNormal = saludo + " " + nombreSaludo;
+let mensaje = `${saludo} ${nombreSaludo} genial`; //Forma de agragar variable a dentro de otra
+console.log(mensaje); 
+console.log(`Hola como vaaaasasaasasa ${nombreSaludo}`);   //Agrega variable a un string, utilizar comillas `...` */
+
+let nombre = prompt("Ingrese su nombre"); //El prompt Muestra un form en el html, se puede guardar en una variable
+let mensaje = `Hola ${nombre}`;
+alert(mensaje);  //Muestra un mensaje-variable cargada, en un recuadro en el html
+
+parseInt();//Convierte strin en numero
